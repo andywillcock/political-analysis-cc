5908117faf7d05ee87505f575fe6330164800990
andywillcock <andy.willcock@gmail.com>
2018-02-12 19:04:57 -0800

    Change percentile calculation to include the passed percent parameter

--- src/extract_repeat_donors.py
+++ src/extract_repeat_donors.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def extract_repeat_donors(record_row,final_set,percent):
     """
-    
+
     :param record_row (dict): Row of data checked for validity by data_check(), stored as a dictionary with column names as keys
     and data as values
     :param final_set (pd.DataFrame): Pandas DataFrame that will store all rows being used to calculate aggregate donations
@@ -14,13 +14,18 @@ def extract_repeat_donors(record_row,final_set,percent):
     record_row['COUNT'] += 1
     final_set = final_set.append(record_row, ignore_index=True)
 
+
+    # Find rows in the filtered data that meet the requirements for output as outlined in the challenge instructions.
     agg_donations = final_set[(final_set['CMTE_ID'] == record_row['CMTE_ID']) &
                     (final_set['ZIP_CODE'] == record_row['ZIP_CODE']) &
                     (final_set['TRANS_DT'] == record_row['TRANS_DT'])]
 
     donation_amounts = sorted(agg_donations.TRANS_AMT.tolist())
-    percentile = donation_amounts[int(round((30/100)*len(donation_amounts)))]
 
+    # Finds the donation closest to the percentile value calculated using the nearest rank method
+    percentile = donation_amounts[int(round((percent/100)*len(donation_amounts)))]
+
+    # Create a numpy array with all relevant information for writing to the ouput file.
     repeated_donations = np.asarray([record_row['CMTE_ID'], record_row['ZIP_CODE'], record_row['TRANS_DT'],
     str(int(round(percentile, 0))),str(int(sum(agg_donations.TRANS_AMT))), str(int(sum(agg_donations.COUNT)))])
 
